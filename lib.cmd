13985c13ac7412b3501d3a27ee16081b5477afad
gn5r <shangyuan.tuolang@gmail.com>
2020-10-09 20:10:02 +0900

    zipファイル名をcommon-utils4jに変更

--- lib.cmd
+++ lib.cmd
@@ -3,5 +3,5 @@
 @REM target下のjavadoc.jar、source.jar、jarファイルをzip圧縮するバッチファイル
 @REM target下のjarファイルを lib.zip ファイル名で圧縮する
 ECHO jarファイルをzip圧縮します
-powershell Compress-Archive -Path ./target/*.jar -DestinationPath lib.zip -Force
+powershell Compress-Archive -Path ./target/*.jar -DestinationPath common-utils4j.zip -Force
 ECHO zip圧縮しました
